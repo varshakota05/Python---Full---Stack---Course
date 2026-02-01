fbf81b9f0fad840e5e18021d65a141ab87e45a1c
Kota Venkata Sai Srivarsha <135219002+varshakota05@users.noreply.github.com>
2026-02-01 20:47:19 +0530

    Add files via upload

---
+++ Day 1/1.print_statement.py
@@ -0,0 +1,10 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Name : Varsha")
+Name : Varsha
+>>> print("Course : Python Full Stack")
+Course : Python Full Stack
+>>> print("Batch : CDG-HYD-PFS-048")
+Batch : CDG-HYD-PFS-048
+>>> print("Location : Hyderabad")
+Location : Hyderabad
